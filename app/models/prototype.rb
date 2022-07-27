7a8d11e38ffe1e267362e7d037d1d870eae5f697
Tsukumo290 <correlophusciliatus290@gmail.com>
2022-07-27 11:46:23 +0900

    修正完了

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,9 +6,5 @@ class Prototype < ApplicationRecord
   validates :title,      presence: true
   validates :catch_copy, presence: true
   validates :concept,    presence: true
-  validates :image,      presence: true, unless: :was_attached?
-
-  def was_attached?
-    self.image.attached?
-  end
+  validates :image,      presence: true
 end
